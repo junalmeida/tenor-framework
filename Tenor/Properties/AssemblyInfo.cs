8fa9e290e786ac06c29da9bde2cab26e46d3dc54
Marcos Junior <junalmeida@gmail.com>
2009-04-04 01:28:09 +0000

    SVN-Revision: 102

--- Tenor/Properties/AssemblyInfo.cs
+++ Tenor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tenor")]
 [assembly: AssemblyProduct("Tenor")]
-[assembly: AssemblyCopyright("GPL")]
+[assembly: AssemblyCopyright("This component is under the MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("0.2.*")]
 //[assembly: AssemblyFileVersion("0.1.0.*")]
