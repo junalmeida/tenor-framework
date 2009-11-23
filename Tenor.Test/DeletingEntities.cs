6dcadae507cde341c63648c4e50c5d155e355bba
Marcos Junior <junalmeida@gmail.com>
2009-11-23 18:59:53 +0000

    Adding Deletion Tests
    SVN-Revision: 180

---
+++ Tenor.Test/DeletingEntities.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+using Tenor.Data;
+using SampleApp.Business.Entities;
+
+#if MSTEST
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tenor.BLL;
+#else
+using TestMethodAttribute = NUnit.Framework.TestAttribute;
+using TestClassAttribute = NUnit.Framework.TestFixtureAttribute;
+#endif
+
+
+namespace Tenor.Test
+{
+    /// <summary>
+    /// Summary description for DeletingEntities
+    /// </summary>
+    [TestClass]
+    public class DeletingEntities
+    {
+        /// <summary>
+        /// Deletes one entity.
+        /// </summary>
+        [TestMethod()]
+        public void DeleteOne()
+        {
+            //Create a person, then deletes it
+
+            Person p = new Person();
+            p.Active = true;
+            p.Name = "Test1";
+            p.Save();
+
+            p.Delete();
+        }
+
+        /// <summary>
+        /// Deletes several records based on a criteria.
+        /// </summary>
+        [TestMethod()]
+        public void DeleteSeveral() 
+        {
+            string[] people = new string[] { "Test1", "Test2", "Test3", "Test4"};
+
+            foreach (string person in people)
+            {
+                Person p = new Person();
+                p.Active = true;
+                p.Name = person;
+                p.Save();
+
+            }
+
+            ConditionCollection cc = new ConditionCollection();
+            cc.Add("Name", "Test%", CompareOperator.Like);
+
+            BLLBase.Delete(typeof(Person), cc);
+
+            Person[] dbPeople = Person.Search(cc, null);
+            if (dbPeople.Length > 0)
+            {
+                Assert.Fail("Instances was not deleted.");
+            }
+        }
+    }
+}
