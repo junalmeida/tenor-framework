835d3adf60c199092c5fc93d898e89a1dc361472
Marcos Junior <junalmeida@gmail.com>
2009-11-24 15:30:29 +0000

    SVN-Revision: 189

--- Tenor.Test/SelectingEntities.cs
+++ Tenor.Test/SelectingEntities.cs
@@ -2,9 +2,9 @@
 using System.Text;
 using System.Collections.Generic;
 
+using SampleApp.Business.Entities;
 #if MSTEST
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SampleApp.Business.Entities;
 #else
 using TestMethodAttribute = NUnit.Framework.TestAttribute;
 using TestClassAttribute = NUnit.Framework.TestFixtureAttribute;
