b13e8031f609c7146e25f0464673b8dcde6a3a94
Marcos Junior <junalmeida@gmail.com>
2010-04-02 14:24:50 +0000

    Fixing long-time resource path bug on web.ui.
    SVN-Revision: 266

--- Tenor.Web.UI/Configuration/Resources.cs
+++ Tenor.Web.UI/Configuration/Resources.cs
@@ -105,103 +105,103 @@ namespace Tenor.Configuration
 
     internal class Resources
     {
+        private const string BasePath = AssemblyWebUIWebControls + ".";
+        public const string JsWindow = BasePath + "Window.Window.js";
 
-        public const string JsWindow = "Tenor.Window.js";
-
-        public const string WindowCssHoverHtc = "Tenor.csshover.htc";
-        public const string WindowBottomLeftGif = "Tenor.bottom-left.gif";
-        public const string WindowBottomRightGif = "Tenor.bottom-right.gif";
-        public const string WindowBottomGif = "Tenor.bottom.gif";
-        public const string WindowCloseUpGif = "Tenor.close-up.gif";
-        public const string WindowCloseGif = "Tenor.close.gif";
-        public const string WindowLeftGif = "Tenor.left.gif";
-        public const string WindowRightGif = "Tenor.right.gif";
-        public const string WindowTitleLeftGif = "Tenor.title-left.gif";
-        public const string WindowTitleRightGif = "Tenor.title-right.gif";
-        public const string WindowTitleGif = "Tenor.title.gif";
+        public const string WindowCssHoverHtc = BasePath + "Window.csshover.htc";
+        public const string WindowBottomLeftGif = BasePath + "Window.janela.bottom-left.gif";
+        public const string WindowBottomRightGif = BasePath + "Window.janela.bottom-right.gif";
+        public const string WindowBottomGif = BasePath + "Window.janela.bottom.gif";
+        public const string WindowCloseUpGif = BasePath + "Window.janela.close-up.gif";
+        public const string WindowCloseGif = BasePath + "Window.janela.close.gif";
+        public const string WindowLeftGif = BasePath + "Window.janela.left.gif";
+        public const string WindowRightGif = BasePath + "Window.janela.right.gif";
+        public const string WindowTitleLeftGif = BasePath + "Window.janela.title-left.gif";
+        public const string WindowTitleRightGif = BasePath + "Window.janela.title-right.gif";
+        public const string WindowTitleGif = BasePath + "Window.janela.title.gif";
 
 
 
-        public const string JsCalendar = "Tenor.calendar.js";
-        public const string JsCalendarEn = "Tenor.calendar-en.js";
-        public const string JsCalendarBr = "Tenor.calendar-br.js";
-        public const string JsCalendarSetup = "Tenor.calendar-setup.js";
+        public const string JsCalendar = BasePath + "TextBox.Calendar.calendar.js";
+        public const string JsCalendarEn = BasePath + "TextBox.Calendar.calendar-en.js";
+        public const string JsCalendarBr = BasePath + "TextBox.Calendar.calendar-br.js";
+        public const string JsCalendarSetup = BasePath + "TextBox.Calendar.calendar-setup.js";
 
-        public const string CalendarCalDJpeg = "Tenor.cal-d.jpg";
-        public const string CalendarCalHJpeg = "Tenor.cal-h.jpg";
-        public const string CalendarCalNJpeg = "Tenor.cal-n.jpg";
-        public const string CalendarWin2kCss = "Tenor.calendar-win2k-2.css";
-        public const string CalendarSystemCss = "Tenor.calendar-system.css";
+        public const string CalendarCalDJpeg = BasePath + "TextBox.Calendar.XPStyle.cal-d.jpg";
+        public const string CalendarCalHJpeg = BasePath + "TextBox.Calendar.XPStyle.cal-h.jpg";
+        public const string CalendarCalNJpeg = BasePath + "TextBox.Calendar.XPStyle.cal-n.jpg";
+        public const string CalendarWin2kCss = BasePath + "TextBox.Calendar.W2KStyle.calendar-win2k-2.css";
+        public const string CalendarSystemCss = BasePath + "TextBox.Calendar.calendar-system.css";
 
 
         public const string AssemblyWebUIWebControls = "Tenor.Web.UI.WebControls";
         public const string AssemblyTinyMCE = "Tenor.TinyMCE";
 
-        public const string JsScrollPanel = "Tenor.scrollpanel.js";
-        public const string JsSlidingPanel = "Tenor.slidingpanel.js";
-        public const string JsSortable = "Tenor.sortable.js";
+        public const string JsScrollPanel = BasePath + "ScrollPanel.scrollpanel.js";
+        public const string JsSlidingPanel = BasePath + "SlidingPanel.slidingpanel.js";
+        public const string JsSortable = BasePath + "SortableBulletedList.sortable.js";
 
 
-        public const string JsDropdown = "Tenor.dropdown.js";
-        public const string JsMasks = "Tenor.masks.js";
-        public const string JsMasks2 = "Tenor.masks2.js";
-        public const string JsJQuery = "Tenor.jQuery.js";
-        public const string JsRightClick = "Tenor.rightclick.js";
-        public const string JsSelect = "Tenor.select.js";
+        public const string JsDropdown = BasePath + "ActionDropDown.dropdown.js";
+        public const string JsMasks = BasePath + "TextBox.masks.js";
+        public const string JsMasks2 = BasePath + "TextBox.masks2.js";
+        public const string JsJQuery = BasePath + "TextBox.jQuery.js";
+        public const string JsRightClick = BasePath + "ScriptManager.rightclick.js";
+        public const string JsSelect = BasePath + "ScriptManager.select.js";
         public const string JsTracking = "Tenor.tracking.js";
-        public const string JsIe7Core = "Tenor.ie7-core.js";
-        public const string JsIe7CSS2 = "Tenor.ie7-css2-selectors.js";
+        public const string JsIe7Core = BasePath + "ScriptManager.iefix.ie7-core.js";
+        public const string JsIe7CSS2 = BasePath + "ScriptManager.iefix.ie7-css2-selectors.js";
 
-        public const string JsAdapterUtils = "Tenor.AdapterUtils.js";
+        public const string JsAdapterUtils = BasePath + "Adapters.AdapterUtils.js";
 
-        public const string JsCheckBoxList = "Tenor.checkboxlist.js";
+        public const string JsCheckBoxList = BasePath + "CheckBox.checkboxlist.js";
 
-        public const string JsContextMenoo = "Tenor.contextMenoo.js";
-        public const string JsContextMenooSrc = "Tenor.contextMenoo-src.js";
-
-
-        public const string FlashBmp = "Tenor.flash.bmp";
-        public const string FlashJpg = "Tenor.flash.jpg";
-        public const string JsFlash = "Tenor.flash.js";
-        public const string JsAC_OETags = "Tenor.AC_OETags.js";
+        public const string JsContextMenoo = BasePath + "ContextMenu.contextMenoo.js";
+        public const string JsContextMenooSrc = BasePath + "ContextMenu.contextMenoo-src.js";
 
 
-        public const string JsFloatingPanel = "Tenor.floatingpanel.js";
-
-        public const string JsPreviewImage = "Tenor.previewimage.js";
-        public const string PreviewBlackWhiteGif = "Tenor.black-white.gif";
-        public const string PreviewWhiteBlackGif = "Tenor.white-black.gif";
-        public const string PreviewGrayGif = "Tenor.gray.gif";
-        public const string PreviewPrevCloseGif = "Tenor.prev-close.jpg";
+        public const string FlashBmp = BasePath + "Flash.flash.bmp";
+        public const string FlashJpg = BasePath + "Flash.flash.jpg";
+        public const string JsFlash = BasePath + "Flash.flash.js";
+        public const string JsAC_OETags = BasePath + "Flash.AC_OETags.js";
 
 
-        public const string JsReorderButton = "Tenor.reorderbutton.js";
-        public const string JsResizablePanels = "Tenor.resizablepanels.js";
+        public const string JsFloatingPanel = BasePath + "FloatingPanel.floatingpanel.js";
+
+        public const string JsPreviewImage = BasePath + "PreviewImage.previewimage.js";
+        public const string PreviewBlackWhiteGif = BasePath + "PreviewImage.black-white.gif";
+        public const string PreviewWhiteBlackGif = BasePath + "PreviewImage.white-black.gif";
+        public const string PreviewGrayGif = BasePath + "PreviewImage.gray.gif";
+        public const string PreviewPrevCloseGif = BasePath + "PreviewImage.prev-close.jpg";
 
 
-        public const string JsMooTools1_11_Full = "Tenor.mootools-release-1.11-Full.js";
-        public const string JsMooTools1_11_Core = "Tenor.mootools-release-1.11-Core.js";
-        public const string JsMooTools1_11_CoreTips = "Tenor.mootools-release-1.11-CoreTips.js";
-        public const string JsMooTools1_11_CoreSortables = "Tenor.mootools-release-1.11-CoreSortables.js";
-        public const string JsMooTools1_11_CoreSmoothscroll = "Tenor.mootools-release-1.11-CoreSmoothscroll.js";
-        public const string JsMooTools1_11_CoreRemote = "Tenor.mootools-release-1.11-CoreRemote.js";
-        public const string JsMooTools1_11_CoreEffectsDrag = "Tenor.mootools-release-1.11-CoreEffectsDrag.js";
-        public const string JsMooTools1_11_SortableOrder = "Tenor.sortableorder.js";
-        public const string JsMooTools1_11_SortableOrderSrc = "Tenor.sortableorder-src.js";
-        public const string JsMooSlimbox = "Tenor.slimbox.js";
-
-        public const string JsMooSqueezebox = "Tenor.squeezebox.js";
-        public const string JsMooSqueezeboxSrc = "Tenor.squeezebox-src.js";
-        public const string MooSqueezeboxCss = "Tenor.squeezebox.css";
-        public const string MooSqueezeboxClosebox = "Tenor.closebox.png";
-        public const string MooSqueezeboxSpinner = "Tenor.spinner.png";
+        public const string JsReorderButton = BasePath + "ReorderButton.reorderbutton.js";
+        public const string JsResizablePanels = BasePath + "ResizablePanels.resizablepanels.js";
 
 
-        public const string MooRoot111 = "Tenor.mootools-release-1.11-";
-        public const string MooRoot12 = "Tenor.mootools-1.2-";
+        public const string JsMooTools1_11_Full = BasePath + "ScriptManager.mootools.mootools-release-1.11-Full.js";
+        public const string JsMooTools1_11_Core = BasePath + "ScriptManager.mootools.mootools-release-1.11-Core.js";
+        public const string JsMooTools1_11_CoreTips = BasePath + "ScriptManager.mootools.mootools-release-1.11-CoreTips.js";
+        public const string JsMooTools1_11_CoreSortables = BasePath + "ScriptManager.mootools.mootools-release-1.11-CoreSortables.js";
+        public const string JsMooTools1_11_CoreSmoothscroll = BasePath + "ScriptManager.mootools.mootools-release-1.11-CoreSmoothscroll.js";
+        public const string JsMooTools1_11_CoreRemote = BasePath + "ScriptManager.mootools.mootools-release-1.11-CoreRemote.js";
+        public const string JsMooTools1_11_CoreEffectsDrag = BasePath + "ScriptManager.mootools.mootools-release-1.11-CoreEffectsDrag.js";
+        public const string JsMooTools1_11_SortableOrder = BasePath + "ScriptManager.mootools.sortableorder.js";
+        public const string JsMooTools1_11_SortableOrderSrc = BasePath + "ScriptManager.mootools.sortableorder-src.js";
+        public const string JsMooSlimbox = BasePath + "ScriptManager.mootools.slimbox.js";
 
-        public const string JsMooTools1_2_Full = "Tenor.mootools-1.2-Full.js";
-        public const string JsMooTools1_2_Core = "Tenor.mootools-1.2-Core.js";
+        public const string JsMooSqueezebox = BasePath + "Tenor.squeezebox.js";
+        public const string JsMooSqueezeboxSrc = BasePath + "Tenor.squeezebox-src.js";
+        public const string MooSqueezeboxCss = BasePath + "Tenor.squeezebox.css";
+        public const string MooSqueezeboxClosebox = BasePath + "Tenor.closebox.png";
+        public const string MooSqueezeboxSpinner = BasePath + "Tenor.spinner.png";
+
+
+        public const string MooRoot111 = BasePath + "ScriptManager.mootools.mootools-release-1.11-";
+        public const string MooRoot12 = BasePath + "ScriptManager.mootools.mootools-1.2-";
+
+        public const string JsMooTools1_2_Full = BasePath + "ScriptManager.mootools.mootools-1.2-Full.js";
+        public const string JsMooTools1_2_Core = BasePath + "ScriptManager.mootools.mootools-1.2-Core.js";
 
     }
 }
